38211b74885bcaecb04f842c799ad8a341ad39b9
Victor Fung <victor.fung@touchcast.co.nz>
2017-03-07 15:15:26 +1300

    travis ci

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 source ${DIR}/vars.sh
 
-docker exec -it ${WEB_CONTAINER_ID} composer update
+docker exec -it ${WEB_CONTAINER_ID} composer install
